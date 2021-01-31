67554958db3563a48ee0cbf570049ae14f518b1e
adamws <adamws@users.noreply.github.com>
2021-01-31 16:50:39 +0100

    improve routing

--- keyautoplace.py
+++ keyautoplace.py
@@ -6,7 +6,7 @@ import sys
 import json
 import itertools
 import logging
-
+import re
 
 class KeyPlacer():
     def __init__(self, logger, board, layout):
@@ -50,7 +50,7 @@ class KeyPlacer():
         track.SetWidth(FromMM(0.25))
         track.SetLayer(layer)
         track.SetStart(start)
-        segmentEnd = wxPoint(track.GetStart().x + FromMM(vector[0]), track.GetStart().y + FromMM(vector[1]))
+        segmentEnd = wxPoint(track.GetStart().x + vector[0], track.GetStart().y + vector[1])
         track.SetEnd(segmentEnd)
 
         layerName = self.board.GetLayerName(layer)
@@ -60,30 +60,48 @@ class KeyPlacer():
         track.SetLocked(True)
         return segmentEnd
 
-    def RouteKeyWithDiode(self, key, diode):
-        end = self.AddTrackSegment(diode.FindPadByName('2').GetPosition(), [-1.98, -1.98])
-        self.AddTrackSegment(end, [0, -4.2])
+    def AddTrackSegmentByPoints(self, start, stop, layer=B_Cu):
+        track = TRACK(self.board)
+        track.SetWidth(FromMM(0.25))
+        track.SetLayer(layer)
+        track.SetStart(start)
+        track.SetEnd(stop)
 
-    def RouteColumn(self, key):
-        segmentStart = wxPoint(key.GetPosition().x - FromMM(3.11), key.GetPosition().y - FromMM(1.84))
-        self.AddTrackSegment(segmentStart, [0, 10], layer=F_Cu)
+        layerName = self.board.GetLayerName(layer)
+        self.logger.info("Adding track segment ({}): [{}, {}]".format(layerName, start, stop))
+        self.board.Add(track)
+
+        track.SetLocked(True)
+        return stop
+
+    def RouteSwitchWithDiode(self, switch, diode):
+        switchPadPosition = switch.FindPadByName("2").GetPosition()
+        diodePadPosition = diode.FindPadByName("2").GetPosition()
+        x_diff = abs(diodePadPosition.x - switchPadPosition.x)
+        # first segment: at 45 degree angle towards switch pad
+        end = self.AddTrackSegment(diodePadPosition, [-x_diff, -x_diff])
+        # second segment: up to switch pad
+        self.AddTrackSegment(end, [0, -abs(end.y - switchPadPosition.y)])
+
+    def RouteColumn(self, switch):
+        switchPadPosition = switch.FindPadByName("1").GetPosition()
+        self.AddTrackSegment(switchPadPosition, [0, FromMM(8)], layer=F_Cu)
 
     def Rotate(self, module, rotationReference, angle):
         self.logger.info("Rotating {} module: rotationReference: {}, rotationAngle: {}".format(module.GetReference(), rotationReference, angle))
         module.Rotate(rotationReference, angle * -10)
 
     def Run(self, keyFormat, stabilizerFormat, diodeFormat, routeTracks=False):
+        column_switch_pads = {}
+        row_diode_pads = {}
         for key in self.layout["keys"]:
-            keyModule, stabilizer = self.GetCurrentKey(keyFormat, stabilizerFormat)
+            switchModule, stabilizer = self.GetCurrentKey(keyFormat, stabilizerFormat)
 
             position = wxPoint((self.keyDistance * key["x"]) + (self.keyDistance * key["width"] // 2),
                 (self.keyDistance * key["y"]) + (self.keyDistance * key["height"] // 2)) + self.referenceCoordinate
-            self.SetPosition(keyModule, position)
+            self.SetPosition(switchModule, position)
             if stabilizer:
                 self.SetPosition(stabilizer, position)
-
-            diodeModule = self.GetCurrentDiode(diodeFormat)
-            self.SetRelativePositionMM(diodeModule, position, [5.08, 3.03])
 
             diodeModule = self.GetCurrentDiode(diodeFormat)
             self.SetRelativePositionMM(diodeModule, position, [5.08, 3.03])
@@ -91,22 +109,69 @@ class KeyPlacer():
             angle = key["rotation_angle"]
             if angle != 0:
                 rotationReference = wxPoint((self.keyDistance * key["rotation_x"]), (self.keyDistance * key["rotation_y"])) + self.referenceCoordinate
-                self.Rotate(keyModule, rotationReference, angle)
+                self.Rotate(switchModule, rotationReference, angle)
                 if stabilizer:
                     self.Rotate(stabilizer, rotationReference, angle)
                 self.Rotate(diodeModule, rotationReference, angle)
                 if not diodeModule.IsFlipped():
                     diodeModule.Flip(diodeModule.GetPosition())
-                diodeModule.SetOrientationDegrees(keyModule.GetOrientationDegrees() - 270)
+                diodeModule.SetOrientationDegrees(switchModule.GetOrientationDegrees() - 270)
             else:
                 if diodeModule.GetOrientationDegrees() != 90.0:
                     diodeModule.SetOrientationDegrees(270)
                 if not diodeModule.IsFlipped():
                     diodeModule.Flip(diodeModule.GetPosition())
 
-            if routeTracks == True:
-                self.RouteKeyWithDiode(keyModule, diodeModule)
-                self.RouteColumn(keyModule)
+            # append pad:
+            pad = switchModule.FindPadByName("1")
+            net_name = pad.GetNetname()
+            match = re.match(r"^COL(\d+)$", net_name)
+            if match:
+                column_number = match.groups()[0]
+                column_switch_pads.setdefault(column_number, []).append(pad)
+            else:
+                self.logger.warning("Switch pad without recognized net name found.")
+            # append diode:
+            pad = diodeModule.FindPadByName("1")
+            net_name = pad.GetNetname()
+            match = re.match(r"^ROW(\d+)$", net_name)
+            if match:
+                row_number = match.groups()[0]
+                row_diode_pads.setdefault(row_number, []).append(pad)
+            else:
+                self.logger.warning("Switch pad without recognized net name found.")
+
+            if routeTracks:
+                self.RouteSwitchWithDiode(switchModule, diodeModule)
+
+        if routeTracks:
+            # very naive routing approach, will fail in some scenarios:
+            for column in column_switch_pads:
+                pads = column_switch_pads[column]
+                positions = [pad.GetPosition() for pad in pads]
+                for pos1, pos2 in zip(positions, positions[1:]):
+                    # connect two pads together
+                    if pos1.x == pos2.x:
+                        self.AddTrackSegmentByPoints(pos1, pos2, layer=F_Cu)
+                    else:
+                        # two segment track
+                        y_diff = abs(pos1.y - pos2.y)
+                        x_diff = abs(pos1.x - pos2.x)
+                        vector = [0, (y_diff - x_diff)]
+                        if vector[1] <= 0:
+                            self.logger.warning("Switch pad to far to route 2 segment track with 45 degree angles")
+                        else:
+                            lastPosition = self.AddTrackSegment(pos1, vector, layer=F_Cu)
+                            self.AddTrackSegmentByPoints(lastPosition, pos2, layer=F_Cu)
+
+            for row in row_diode_pads:
+                pads = row_diode_pads[row]
+                positions = [pad.GetPosition() for pad in pads]
+                for pos1, pos2 in zip(positions, positions[1:]):
+                    if pos1.y == pos2.y:
+                        self.AddTrackSegmentByPoints(pos1, pos2)
+                    else:
+                        self.logger.warning("Automatic diode routing supported only when diodes aligned vertically")
 
 
 class KeyAutoPlaceDialog(wx.Dialog):
@@ -233,10 +298,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Keyboard's key autoplacer")
     parser.add_argument('-l', '--layout', required=True, help="json layout definition file")
     parser.add_argument('-b', '--board', required=True, help=".kicad_pcb file to be processed")
+    parser.add_argument('-r', '--route', action="store_true", help="Enable experimental routing")
 
     args = parser.parse_args()
     layoutPath = args.layout
     boardPath = args.board
+    routeTracks = args.route
 
     # set up logger
     logging.basicConfig(level=logging.DEBUG,
@@ -252,7 +319,7 @@ if __name__ == "__main__":
 
     board = LoadBoard(boardPath)
     placer = KeyPlacer(logger, board, layout)
-    placer.Run("SW{}", "ST{}", "D{}", False)
+    placer.Run("SW{}", "ST{}", "D{}", routeTracks)
 
     Refresh()
     SaveBoard(boardPath, board)
