588a1b11c02a46ab7b54d409c1d6d1a4e0bc9856
adamws <adamws@users.noreply.github.com>
2023-06-21 23:23:26 +0200

    Add automatic repository deployment to gh-pages branch

---
+++ release/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -o nounset
+set -o errexit
+
+cd output
+
+echo "==> Prepare to deploy\n"
+git init
+git config --global user.name "CircleCI"
+git config --global user.email "${CIRCLE_PROJECT_USERNAME}@users.noreply.github.com"
+
+if [ -z "$(git status --porcelain)" ]; then
+    echo "Something went wrong" && \
+    echo "Exiting..."
+    exit 0
+fi
+
+mkdir .circleci
+wget https://raw.githubusercontent.com/adamws/kicad-kbplacer/develop/.circleci/ghpages-config.yml -O .circleci/config.yml
+touch .nojekyll
+
+echo "==> Start deploying"
+git add -A
+git commit -m "Deploy plugin repository: ${CIRCLE_SHA1}"
+
+git push --force $CIRCLE_REPOSITORY_URL master:gh-pages
+
+rm -fr .git
+
+echo "==> Deploy succeeded"
