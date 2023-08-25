8bb61f15ce87b06e7a778ca05f81e97659dd50ec
adamws <adamws@users.noreply.github.com>
2023-08-25 18:38:59 +0200

    Determine if called by KiCad by inspecting stack

--- kbplacer/__init__.py
+++ kbplacer/__init__.py
@@ -1,6 +1,19 @@
-import sys
+import inspect
 
-if sys.argv[0] != "-m" and not sys.argv[0].endswith("pytest"):
+
+def __is_in_call_stack(function_name: str, module_name: str) -> bool:
+    current_stack = inspect.stack()
+
+    for frame_info in current_stack:
+        frame = frame_info.frame
+        if frame.f_globals.get("__name__") == module_name:
+            if function_name in frame.f_locals or function_name in frame.f_globals:
+                return True
+
+    return False
+
+
+if __is_in_call_stack("LoadPluginModule", "pcbnew"):
     from .kbplacer_plugin_action import KbplacerPluginAction
 
     KbplacerPluginAction().register()
