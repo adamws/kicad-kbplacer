5857181ff7af8b3f40f596f997a100cc83e31e57
adamws <adamws@users.noreply.github.com>
2023-11-15 18:31:07 +0100

    Update kicad 7 docker images to new kicad release

---
+++ .circleci/images/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# helper script for rebuilding images after kicad version update
+# update tag values and Dockerfiles before usage
+
+set -o nounset
+set -o errexit
+
+CUR_DIR="$(dirname $(readlink -f "$0"))"
+
+build_and_push() {
+  docker build -t admwscki/kicad-kbplacer-primary:$1 \
+    --no-cache --progress=plain -f $CUR_DIR/$2 .
+  docker push admwscki/kicad-kbplacer-primary:$1
+}
+
+build_and_push 7.0.9-focal focal/kicad7.Dockerfile
+build_and_push 7.0.9-lunar lunar/kicad7.Dockerfile
--- docker/tests.Dockerfile
+++ docker/tests.Dockerfile
@@ -1,4 +1,4 @@
-FROM admwscki/kicad-kbplacer-primary:7.0.6-lunar
+FROM admwscki/kicad-kbplacer-primary:7.0.9-lunar
 
 COPY dev-requirements.txt .
 RUN pip3 install --no-cache-dir -r dev-requirements.txt && rm dev-requirements.txt
