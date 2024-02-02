9fdbfc81ad49bb5558e216de150c31f3faa83aa2
adamws <adamws@users.noreply.github.com>
2024-02-02 17:27:09 +0100

    Add tests for action plugin `Initialize`

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -2,11 +2,25 @@ import logging
 import os
 import sys
 from pathlib import Path
+from unittest.mock import MagicMock, patch
 
 import pcbnew
 import pytest
 
+try:
+    from kbplacer.kbplacer_plugin_action import KbplacerPluginAction
+except:
+    pass
+
 logger = logging.getLogger(__name__)
+
+
+@pytest.fixture()
+def kbplacer_plugin_action():
+    action = KbplacerPluginAction()
+    action.register()
+    yield action
+    action.deregister()
 
 
 @pytest.mark.run_first
@@ -23,13 +37,35 @@ def test_if_plugin_loads() -> None:
 
 
 @pytest.mark.skipif(sys.platform == "win32", reason="fails on windows")
-def test_if_plugin_registers() -> None:
-    from kbplacer.kbplacer_plugin_action import KbplacerPluginAction
-
-    action = KbplacerPluginAction()
-    action.register()
+def test_if_plugin_registers(kbplacer_plugin_action) -> None:
     dirname = Path(os.path.realpath(__file__)).parents[1]
     assert (
-        action.GetPluginPath()
+        kbplacer_plugin_action.GetPluginPath()
         == f"{dirname}/kbplacer/kbplacer_plugin_action.py/KbplacerPluginAction"
     )
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="fails on windows")
+@patch("pcbnew.GetBoard")
+def test_if_plugin_initializes_with_board(
+    mock_get_board: MagicMock, tmpdir, kbplacer_plugin_action
+) -> None:
+    mock_board = MagicMock()
+    mock_board.GetFileName.return_value = f"{tmpdir}/test_board.kicad_pcb"
+
+    mock_get_board.return_value = mock_board
+    kbplacer_plugin_action.Initialize()
+    assert Path(tmpdir / "kbplacer.log").is_file()
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="fails on windows")
+@patch("pcbnew.GetBoard")
+def test_if_plugin_not_initializes_without_board(
+    mock_get_board: MagicMock, kbplacer_plugin_action
+) -> None:
+    mock_board = MagicMock()
+    mock_board.GetFileName.return_value = ""
+
+    mock_get_board.return_value = mock_board
+    with pytest.raises(Exception, match="Could not locate .kicad_pcb file.*"):
+        kbplacer_plugin_action.Initialize()
