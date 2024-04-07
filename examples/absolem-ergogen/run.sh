083d2f85d6d0e6f8ba0a0fab297afad7b8d08369
adamws <adamws@users.noreply.github.com>
2024-04-07 21:31:33 +0200

    Use current revision when building absolem example on CI

--- examples/absolem-ergogen/run.sh
+++ examples/absolem-ergogen/run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-docker build -t absolem .
+if [ -z "$CIRCLE_SHA1" ]; then
+  KBPLACER_REVISION=master
+else
+  KBPLACER_REVISION=$CIRCLE_SHA1
+fi
+docker build --build-arg="KBPLACER_REVISION=$KBPLACER_REVISION" -t absolem .
 docker cp $(docker create --name absolem absolem:latest /bin/sh):/absolem.zip .
 docker rm absolem
