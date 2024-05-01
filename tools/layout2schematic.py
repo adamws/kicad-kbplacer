f91f8f0f6b6d4a227d8a7956e79322a9a7bfea78
adamws <adamws@users.noreply.github.com>
2024-05-01 19:13:53 +0200

    Fix alternative switches connectivity problems
    There are three slots for switches per matrix position but due to too
    little space, the third one would not correctly connect to wire
    (it would end up in the diode symbol making them unconnected).
    Make more space to fix connections and add missing junction elements.

--- tools/layout2schematic.py
+++ tools/layout2schematic.py
@@ -16,7 +16,7 @@ ORIGIN = (18, 18)
 UNIT = 1.27
 
 COLUMN_DISTANCE = 10
-ROW_DISTANCE = 14
+ROW_DISTANCE = 16
 
 TEMPLATE = """(kicad_sch (version 20230121) (generator eeschema) (uuid 9e45a776-7007-48ff-b543-dc98423173b7) (paper "A4")(lib_symbols (symbol "Device:D_Small" (pin_numbers hide) (pin_names (offset 0.254) hide) (in_bom yes) (on_board yes) (property "Reference" "D" (at -1.27 2.032 0) (effects (font (size 1.27 1.27)) (justify left))) (property "Value" "D_Small" (at -3.81 -2.032 0) (effects (font (size 1.27 1.27)) (justify left))) (property "Footprint" "" (at 0 0 90) (effects (font (size 1.27 1.27)) hide)) (property "Datasheet" "~" (at 0 0 90) (effects (font (size 1.27 1.27)) hide)) (property "Sim.Device" "D" (at 0 0 0) (effects (font (size 1.27 1.27)) hide)) (property "Sim.Pins" "1=K 2=A" (at 0 0 0) (effects (font (size 1.27 1.27)) hide)) (property "ki_keywords" "diode" (at 0 0 0) (effects (font (size 1.27 1.27)) hide)) (property "ki_description" "Diode, small symbol" (at 0 0 0) (effects (font (size 1.27 1.27)) hide)) (property "ki_fp_filters" "TO-???* *_Diode_* *SingleDiode* D_*" (at 0 0 0) (effects (font (size 1.27 1.27)) hide)) (symbol "D_Small_0_1" (polyline (pts (xy -0.762 -1.016) (xy -0.762 1.016)) (stroke (width 0.254) (type default)) (fill (type none))) (polyline (pts (xy -0.762 0) (xy 0.762 0)) (stroke (width 0) (type default)) (fill (type none))) (polyline (pts (xy 0.762 -1.016) (xy -0.762 0) (xy 0.762 1.016) (xy 0.762 -1.016)) (stroke (width 0.254) (type default)) (fill (type none)))) (symbol "D_Small_1_1" (pin passive line (at -2.54 0 0) (length 1.778) (name "K" (effects (font (size 1.27 1.27)))) (number "1" (effects (font (size 1.27 1.27))))) (pin passive line (at 2.54 0 180) (length 1.778) (name "A" (effects (font (size 1.27 1.27)))) (number "2" (effects (font (size 1.27 1.27))))))) (symbol "Switch:SW_Push_45deg" (pin_numbers hide) (pin_names (offset 1.016) hide) (in_bom yes) (on_board yes) (property "Reference" "SW" (at 3.048 1.016 0) (effects (font (size 1.27 1.27)) (justify left))) (property "Value" "SW_Push_45deg" (at 0 -3.81 0) (effects (font (size 1.27 1.27)))) (property "Footprint" "" (at 0 0 0) (effects (font (size 1.27 1.27)) hide)) (property "Datasheet" "~" (at 0 0 0) (effects (font (size 1.27 1.27)) hide)) (property "ki_keywords" "switch normally-open pushbutton push-button" (at 0 0 0) (effects (font (size 1.27 1.27)) hide)) (property "ki_description" "Push button switch, normally open, two pins, 45° tilted" (at 0 0 0) (effects (font (size 1.27 1.27)) hide)) (symbol "SW_Push_45deg_0_1" (circle (center -1.1684 1.1684) (radius 0.508) (stroke (width 0) (type default)) (fill (type none))) (polyline (pts (xy -0.508 2.54) (xy 2.54 -0.508)) (stroke (width 0) (type default)) (fill (type none))) (polyline (pts (xy 1.016 1.016) (xy 2.032 2.032)) (stroke (width 0) (type default)) (fill (type none))) (polyline (pts (xy -2.54 2.54) (xy -1.524 1.524) (xy -1.524 1.524)) (stroke (width 0) (type default)) (fill (type none))) (polyline (pts (xy 1.524 -1.524) (xy 2.54 -2.54) (xy 2.54 -2.54) (xy 2.54 -2.54)) (stroke (width 0) (type default)) (fill (type none))) (circle (center 1.143 -1.1938) (radius 0.508) (stroke (width 0) (type default)) (fill (type none))) (pin passive line (at -2.54 2.54 0) (length 0) (name "1" (effects (font (size 1.27 1.27)))) (number "1" (effects (font (size 1.27 1.27))))) (pin passive line (at 2.54 -2.54 180) (length 0) (name "2" (effects (font (size 1.27 1.27)))) (number "2" (effects (font (size 1.27 1.27)))))))) (symbol (lib_id "Switch:SW_Push_45deg") (at 0 0 0) (unit 1) (in_bom yes) (on_board yes) (dnp no) (uuid 19751ded-3cc5-4b31-aeeb-fd1357dc1d55) (property "Reference" "SW1" (at 0 -5.08 0) (effects (font (size 1.27 1.27)))) (property "Value" "SW_Push" (at 0 -3.81 0) (effects (font (size 1.27 1.27)) hide)) (property "Footprint" "" (at 0 0 0) (effects (font (size 1.27 1.27)) hide)) (property "Datasheet" "~" (at 0 0 0) (effects (font (size 1.27 1.27)) hide)) (pin "1" (uuid 94c53fa6-dd8d-4e6d-9c1a-277431558d0a)) (pin "2" (uuid ef827767-19b4-4ee5-b4b6-65189b88f8ee)) (instances (project "template" (path "/9e45a776-7007-48ff-b543-dc98423173b7" (reference "SW1") (unit 1))))) (symbol (lib_id "Device:D_Small") (at 2.54 6.35 90) (unit 1) (in_bom yes) (on_board yes) (dnp no) (uuid feb0fa1f-f7ed-4393-bef6-632a7fa048d6) (property "Reference" "D1" (at 3.81 5.08 90) (effects (font (size 1.27 1.27)) (justify right))) (property "Value" "D" (at 3.81 7.62 90) (effects (font (size 1.27 1.27)) (justify right) hide)) (property "Footprint" "" (at 2.54 6.35 90) (effects (font (size 1.27 1.27)) hide)) (property "Datasheet" "~" (at 2.54 6.35 90) (effects (font (size 1.27 1.27)) hide)) (property "Sim.Device" "D" (at 2.54 6.35 0) (effects (font (size 1.27 1.27)) hide)) (property "Sim.Pins" "1=K 2=A" (at 2.54 6.35 0) (effects (font (size 1.27 1.27)) hide)) (pin "1" (uuid 004cc590-2791-46a0-9480-7962783605a4)) (pin "2" (uuid 428af732-f843-4705-863e-a0095a5fb80a)) (instances (project "template" (path "/9e45a776-7007-48ff-b543-dc98423173b7" (reference "D1") (unit 1))))) (sheet_instances (path "/" (page "1"))))"""
 
@@ -117,6 +117,9 @@ def create_schematic(
         switch_x = _x(COLUMN_DISTANCE * int(column) + 5)
         switch_y = _y(ROW_DISTANCE * int(row) + used_slots)
         switch.move(switch_x, switch_y)
+        if used_slots != 0:
+            junc = sch.junction.new()
+            junc.move(switch.pin.n2.location.x, switch.pin.n2.location.y)
         wire = sch.wire.new()
         wire.start_at(switch.pin.n1)
         wire.delta_x = -1 * UNIT
@@ -125,7 +128,7 @@ def create_schematic(
             column_wire = sch.wire.new()
             column_wire.start_at(wire.end)
             column_wire.delta_x = 0
-            column_wire.delta_y = (ROW_DISTANCE * (rows - row - 1) + 13) * UNIT
+            column_wire.delta_y = (ROW_DISTANCE * (rows - row - 1) + 15) * UNIT
 
             label = sch.global_label.new()
             label.move(column_wire.end.value[0], column_wire.end.value[1], 270)
@@ -139,7 +142,7 @@ def create_schematic(
             diode = base_diode.clone()
             diode.setAllReferences(f"D{current_ref}")
             diode_x = switch_x + 2 * UNIT
-            diode_y = switch_y + 5 * UNIT
+            diode_y = switch_y + 7 * UNIT
             diode.move(diode_x, diode_y)
             wire = sch.wire.new()
             wire.start_at(switch.pin.n2)
