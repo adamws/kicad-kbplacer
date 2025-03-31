a73280fef33d3d537dd346a745c2e87b5b3e69ec
adamws <adamws@users.noreply.github.com>
2025-03-31 21:58:55 +0200

    Add plate generation to absolem example

---
+++ examples/absolem-ergogen/generate-plate.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd /kicad-kbplacer
+if hatch run tools:layout2openscad --help; then
+  echo "Running plate generation tool"
+  hatch run tools:layout2openscad \
+    -in $WORK_PATH/$PROJECT_NAME-kle.json \
+    -out $WORK_PATH/$PROJECT_NAME-plate.scad \
+    -shape convex_hull
+  cd -
+  openscad $PROJECT_NAME-plate.scad \
+    -o $PROJECT_NAME-plate.png --viewall --view axes
+  zip -rv $PROJECT_NAME.zip $PROJECT_NAME-plate.png $PROJECT_NAME-plate.scad
+else
+  echo "Plate generation tool not available"
+  cd -
+fi
+
