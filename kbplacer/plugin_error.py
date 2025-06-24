4e4134679490fecfff1c6d230cdd217d7c047256
adamws <adamws@users.noreply.github.com>
2025-06-24 19:28:34 +0200

    Include `PluginError` messages in the log file

--- kbplacer/plugin_error.py
+++ kbplacer/plugin_error.py
@@ -1,9 +1,13 @@
 # SPDX-FileCopyrightText: 2025 adamws <adamws@users.noreply.github.com>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class PluginError(Exception):
     def __init__(self, message: str) -> None:
         self.message = message
+        logger.error(self.message.replace("\n", " "))
         super().__init__(self.message)
